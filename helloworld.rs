1437ea5f48d9473d5731c6571eb6f5cb06513120
Ranxin <rstli@ucdavis.edu>
2024-06-16 22:15:06 -0700

    upload string part

--- helloworld.rs
+++ helloworld.rs
@@ -4,7 +4,14 @@ fn factorial(n:i32)->i32{
     }
     return n*factorial(n-1)
 }
-
+struct Student{
+    name:String, age:i32
+}
+impl Student{
+    fn show(&self){
+        println!("My name is {} and I am {} years old", self.name, self.age)
+    }
+}
 fn main(){
     println!("hello world");
     let name="John";
@@ -32,4 +39,11 @@ fn main(){
     for num in 0..count_arr.len(){
         println!("num: {} {}", num, count_arr[num]);
     }
+    // String part
+    let mut student_name=String::from("John");
+    student_name.push_str(" Smith");
+    let s=Student{name:student_name, age:25};
+    s.show();
+    let last_name=&student_name[5..];
+    println!("My last name is: {}", last_name);
 }
