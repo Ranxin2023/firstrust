d9f6bf08fa8041e00c933f6805057c1a2425a72f
Ranxin <rstli@ucdavis.edu>
2024-06-16 19:12:46 -0700

    add variables and immutable variables

--- helloworld.rs
+++ helloworld.rs
@@ -2,5 +2,13 @@ fn main(){
     println!("hello world");
     let name="John";
     println!("Hello, this is {}", name);
-    
+    // name="tom"; This will cause error
+    let mut mutable_name = "John";
+    println!("Hello, my initial name is {}", mutable_name);
+    mutable_name = "Tom";
+    println!("Hello, this is {}", mutable_name);
+    for i in 1..11{//11 is not inclusive
+        print!("{}\t", i)//This will print without changing a line
+    }
+    println!()
 }
